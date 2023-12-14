85dab40fcb0172ee57cf721e305b38df39e296d0
PPpapnai <113411222+PPpapnai@users.noreply.github.com>
2023-12-14 18:33:55 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,11 @@
-console.log("hello")
+const fs = require('fs');
+console.log("hello1")
+try {
+    const payload1=JSON.parse(fs.readFileSync('./payload.json'))
+    console.log(payload1);
+
+    
+} catch (error) {
+    console.log(error);
+}
+console.log("hello2")
