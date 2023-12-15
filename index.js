2373174c2b8b9e0740215def7d54b5740eb22a0f
PPpapnai <113411222+PPpapnai@users.noreply.github.com>
2023-12-15 18:28:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 console.log("hello1");
-const payload=JSON.parse(process.env.PAYLOAD);
+//const payload=JSON.parse(process.env.PAYLOAD);
+try {
+    const payload1=JSON.parse(fs.readFileSync('./payload.json'))
+    console.log(payload1);
+
+    
+} catch (error) {
+    console.log(error);
+}
 console.log("hello2");
-console.log(payload)
+//console.log(payload)
 
 
 
