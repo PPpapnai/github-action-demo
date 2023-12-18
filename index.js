d3ff1c19e2a42b1116318be5c37ce5cd141c347f
PPpapnai <113411222+PPpapnai@users.noreply.github.com>
2023-12-18 11:56:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 console.log("hello1");
-console.log("webhooks enabled")
+console.log("webhooks enabled once again")
 //const payload=JSON.parse(process.env.PAYLOAD);
 try {
     const payload1=JSON.parse(fs.readFileSync('./payload.json'))
