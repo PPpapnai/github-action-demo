6072210c51679a4655064a1e0722718d157b911c
PPpapnai <113411222+PPpapnai@users.noreply.github.com>
2023-12-28 18:41:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const fs = require('fs');
-console.log("hello1");
-console.log("webhooks enabled once again")
-//const payload=JSON.parse(process.env.PAYLOAD);
-try {
-    const payload1=JSON.parse(fs.readFileSync('./payload.json'))
-    console.log(payload1);
+// const fs = require('fs');
+// console.log("hello1");
+// console.log("webhooks enabled once again")
+const payload=JSON.parse(process.env.PAYLOAD);
+console.log(payload);
+// try {
+//     const payload1=JSON.parse(fs.readFileSync('./payload.json'))
+//     console.log(payload1);
 
     
-} catch (error) {
-    console.log(error);
-}
-console.log("hello2");
-//console.log(payload)
+// } catch (error) {
+//     console.log(error);
+// }
+// console.log("hello2");
+
 
 
 
